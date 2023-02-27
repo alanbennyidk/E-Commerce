91c443da692ba6c84eb4de63b7d3b8c94be1328a
alanbennyidk <alanbennyidk@gmail.com>
2023-02-27 22:15:31 +0530

    first commit

---
+++ src/app/products/cart/cart.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  cartitems:any=[] //cart items
+  grand:any;
+  updatetotal:any;
+  constructor(private cart:CartService,private router:Router) { }
+
+  ngOnInit(): void {
+    this.cart.cartlist.subscribe(
+      (data:any)=>{
+      console.log(data);
+      this.cartitems=data;
+    }
+    )
+    this.grand=this.cart.gettotal()
+  }
+  removeitem(product:any){
+    this.cart.removecart(product)
+    this.grand=this.cart.gettotal()
+
+  }
+  removeall(){
+    this.cart.removeall()
+  }
+  discount3per(){
+    let discount=(this.grand*3)/100
+    this.updatetotal=this.grand-discount
+  }
+  discount10per(){
+    let discount=(this.grand*10)/100
+    this.updatetotal=this.grand-discount
+  }
+  discount30per(){
+    let discount=(this.grand*30)/100
+    this.updatetotal=this.grand-discount
+  }
+  discount50per(){
+    let discount=(this.grand*50)/100
+    this.updatetotal=this.grand-discount
+  }
+
+  proceed(){
+    alert('Your order is placed')
+    this.router.navigateByUrl('')
+    this.removeall()
+  }
+
+
+}
